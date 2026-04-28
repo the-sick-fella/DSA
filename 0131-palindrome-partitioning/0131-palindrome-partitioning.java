743dfc4f8f1ccc3f403cd9c96b9a66905a0ae696
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-04-28 19:58:23 +0530

    Time: 7 ms (97.92%), Space: 65.3 MB (57%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,34 +1,31 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> list = new ArrayList<>();
-        for(int parts = 1; parts<=s.length(); parts++){
-            p(0, parts, list, s, new ArrayList<>());
-        }
+        p(0, list, s, new ArrayList<>());
+
         return list;
     }
 
-    void p(int idx, int parts, List<List<String>> list, String s, List<String> temp){
-        if(parts == 1){
-            if(!valid(s, idx, s.length()-1)) return;
-
-            temp.add(s.substring(idx, s.length()));
+    void p(int idx, List<List<String>> list, String s, List<String> temp) {
+        if (idx >= s.length()) {
             list.add(new ArrayList<>(temp));
-            temp.remove(temp.size() -1);
             return;
         }
 
-        for(int len = 1; idx+len < s.length(); len++){
-            if(!valid(s, idx, idx+len-1)) continue;
+        for (int i = 0; idx + i < s.length(); i++) {
+            if (!valid(s, idx, idx + i))
+                continue;
 
-            temp.add(s.substring(idx, idx+len));
-            p(idx+len, parts-1, list, s, temp);
+            temp.add(s.substring(idx, idx + i + 1));
+            p(idx + i + 1, list, s, temp);
             temp.remove(temp.size() - 1);
         }
     }
 
-    boolean valid(String s, int si, int ei){
-        while(si<ei){
-            if(s.charAt(si) != s.charAt(ei)) return false;
+    boolean valid(String s, int si, int ei) {
+        while (si < ei) {
+            if (s.charAt(si) != s.charAt(ei))
+                return false;
             si++;
             ei--;
         }
