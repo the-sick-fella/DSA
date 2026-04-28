1569325772d594ae8314c34501ac7bdb01a9b031
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-04-28 20:05:38 +0530

    Time: 7 ms (97.92%), Space: 65.6 MB (9.83%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -12,12 +12,12 @@ class Solution {
             return;
         }
 
-        for (int i = 0; idx + i < s.length(); i++) {
-            if (!valid(s, idx, idx + i))
+        for (int i = idx; i < s.length(); i++) {
+            if (!valid(s, idx, i))
                 continue;
 
-            temp.add(s.substring(idx, idx + i + 1));
-            p(idx + i + 1, list, s, temp);
+            temp.add(s.substring(idx, i + 1));
+            p(i + 1, list, s, temp);
             temp.remove(temp.size() - 1);
         }
     }
