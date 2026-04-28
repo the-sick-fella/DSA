7df087913ff88accb79e9b01a269af8d85efad1f
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-04-28 20:19:17 +0530

    Time: 8 ms (73.91%), Space: 65.4 MB (24.52%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -2,23 +2,22 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> list = new ArrayList<>();
         p(0, list, s, new ArrayList<>());
-
         return list;
     }
 
-    void p(int idx, List<List<String>> list, String s, List<String> temp) {
+    void p(int idx, List<List<String>> list, String s, List<String> curr) {
         if (idx >= s.length()) {
-            list.add(new ArrayList<>(temp));
+            list.add(new ArrayList<>(curr));
             return;
         }
 
         for (int i = idx; i < s.length(); i++) {
-            if (!valid(s, idx, i))
-                continue;
+            if (valid(s, idx, i)) {
 
-            temp.add(s.substring(idx, i + 1));
-            p(i + 1, list, s, temp);
-            temp.remove(temp.size() - 1);
+                curr.add(s.substring(idx, i + 1));
+                p(i + 1, list, s, curr);
+                curr.remove(curr.size() - 1);
+            }
         }
     }
 
