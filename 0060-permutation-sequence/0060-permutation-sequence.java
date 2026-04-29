6443a56663f551f9dd81c9e33247d2ee724682a0
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-04-29 22:37:33 +0530

    Time: 1 ms (99.52%), Space: 42.8 MB (74.18%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,53 +1,30 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        List<String> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
 
-        int div = 1;
-        for(int i = 2; i<n; i++){
-            div*=i;
+        int fac = 1;
+        for (int i = 1; i <= n; i++){
+            list.add(i);
+            fac *= i;
         }
 
-        int si, counter;
-        if(k%div != 0){
-            si = (k/div) + 1;
-            counter = k%div;
-        }
-        else{
-            si = k/div;
-            counter = div;
-        }
-
-        boolean x = p(n, counter, true, si, sb, new ArrayList<>(), list);
-        return list.get(list.size()-1);
+        p(n, k-1, sb, list, fac/n);
+        return sb.toString();
     }
 
-    boolean p(int n, int k, boolean first, int si, StringBuilder sb, List<Integer> visited, List<String> list){
-        if(visited.size() == n){
-            list.add(sb.toString());
-
-            if(list.size() == k){
-                return true;
-            }
-            return false;
+    void p(int n, int k, StringBuilder sb, List<Integer> list, int fac) {
+        if(n == 1) {
+            sb.append(list.get(0));
+            return;
         }
 
-        int i;
-        if(first) i = si;
-        else i=1;
-        
-        for(; i<=n; i++){
-            if(visited.contains(i)) continue;
+        int range = k/fac;
+        sb.append(list.get(range));
+        list.remove(range);
 
-            sb.append(i);
-            visited.add(i);
-            boolean found = p(n, k, false, si, sb, visited, list);
-            if(found) return true;
+        int pos = k%fac;
 
-            visited.remove(visited.size() -1);
-            sb.deleteCharAt(sb.length() -1);
-        }
-
-        return false;
+        p(n-1, pos, sb, list, fac/(n-1));
     }
 }
