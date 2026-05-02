639bf17c4a9fb17e6c60bf8246defcd4a94262b7
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-02 15:58:50 +0530

    Time: 2 ms (81.81%), Space: 46.8 MB (28.16%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,62 +1,44 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> list = new ArrayList<>();
-        char [][] board = new char[n][n];
-        for(char[] r : board){
+        char[][] board = new char[n][n];
+        for (char[] r : board) {
             Arrays.fill(r, '.');
         }
-        
-        solve(n, 0, list, board);
+
+        boolean[] columns = new boolean[n];
+        boolean[] ld = new boolean[2*n - 1];
+        boolean[] rd = new boolean[2*n - 1];
+
+        solve(n, 0, list, board, columns, ld, rd);
         return list;
     }
 
-    void solve(int n, int row, List<List<String>> list, char[][] board){
-        if(row >= n){
+    void solve(int n, int row, List<List<String>> list, char[][] board, boolean[] columns, boolean[] ld, boolean[] rd) {
+        if (row >= n) {
             List<String> curr = new ArrayList<>();
-            for(char [] r : board){
+            for (char[] r : board) {
                 curr.add(new String(r));
             }
             list.add(new ArrayList<>(curr));
             return;
         }
 
+        for (int c = 0; c < n; c++) {
+            if (columns[c] || ld[row - c + (n-1)] || rd[row + c])
+                continue;
 
-        for(int c = 0; c<n; c++){
-            if(valid(n, row, c, board)){
-                board[row][c] = 'Q';
-                
-                solve(n, row+1, list, board);
+            board[row][c] = 'Q';
+            columns[c] = true;
+            ld[row - c + n - 1] = true;
+            rd[row + c] = true;
 
-                board[row][c] = '.';
-            }
+            solve(n, row + 1, list, board, columns, ld, rd);
+
+            board[row][c] = '.';
+            columns[c] = false;
+            ld[row - c + n - 1] = false;
+            rd[row + c] = false;
         }
-    }
-
-    boolean valid(int n, int row, int col, char [][] board){
-        //row
-        int r = 0;
-        for(; r<row; r++){
-            if(board[r][col] == 'Q') return false;
-        }
-
-        //left diagonal
-        r = row-1;
-        int c = col - 1;
-        while(r>=0 && c>=0){
-            if(board[r][c] == 'Q') return false;
-            r--;
-            c--;
-        }
-
-        //right diagonal
-        r = row-1;
-        c = col + 1;
-        while(r>=0 && c<n){
-            if(board[r][c] == 'Q') return false;
-            r--;
-            c++;
-        }
-
-        return true;
     }
 }
