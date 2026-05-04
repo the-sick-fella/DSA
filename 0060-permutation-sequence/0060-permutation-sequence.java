ba98fc45cc156d83c98c53f43c933de6092f46e6
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-04 23:16:34 +0530

    Time: 1 ms (99.5%), Space: 42.7 MB (86.95%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,30 +1,28 @@
 class Solution {
     public String getPermutation(int n, int k) {
+        int x = 1;
         List<Integer> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-
-        int fac = 1;
-        for (int i = 1; i < n; i++) {
+        for(int i = 1; i<=n; i++){
+            x *= i;
             list.add(i);
-            fac *= i;
         }
-        list.add(n);
+
+        x/=n;
+        StringBuilder sb = new StringBuilder();
+        return p(n, k-1, list, x, sb);
+    }
+
+    String p(int n, int k, List<Integer> list, int x, StringBuilder sb){
+        if(list.size() == 1){
+            sb.append(list.get(0));
+            return sb.toString();
+        }
+
+        int range = k/x;
+        sb.append(list.get(range));
+        list.remove(range);
         
-        k--;
-        n--;
-
-        while (!list.isEmpty()) {
-            int range = k / fac;
-            sb.append(list.get(range));
-            list.remove(range);
-
-            if(n==0) break;
-            
-            k = k % fac;
-            fac /= n;
-            n--;
-        }
-
-        return sb.toString();
+        int pos = k%x;
+        return p(n-1, pos, list, x/(n-1), sb);
     }
 }
