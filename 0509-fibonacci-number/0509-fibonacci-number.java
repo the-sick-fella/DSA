8f525a379156231562eb6ec0aade7b78b5e881c0
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-07 19:39:50 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.99%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if(n < 2) return n;
+        int dp [] = new int[n+1];
+        Arrays.fill(dp, -1);
+        return f(n, dp);
+    }
 
-        return fib(n-1) + fib(n-2);
+    int f(int n, int [] dp){
+        if(n<2) return n;
+
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = f(n-1, dp) + f(n-2, dp);
+        return dp[n];
     }
 }
