9ed864cb0b7b1660d3935cf6261166f2c50d65d2
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-09 19:50:28 +0530

    Time: 67 ms (0.54%), Space: 43 MB (18.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        int [] dp = new int[nums.length];
+        int dp [] = new int[nums.length];
         Arrays.fill(dp, -1);
+        
         return f(nums, dp, 0);
     }
-
+    
     int f(int [] nums, int [] dp, int idx){
         if(idx >= nums.length) return 0;
-
+        
         if(dp[idx] != -1) return dp[idx];
-
+        
         int take = nums[idx] + f(nums, dp, idx+2);
         int skip = f(nums, dp, idx+1);
-
+        
         return dp[idx] = Math.max(take, skip);
     }
 }
