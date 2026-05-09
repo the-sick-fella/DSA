a9cc6a90e2aea3abde6874443f6097e6fc532fb5
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-09 18:01:26 +0530

    Time: 0 ms (100%), Space: 42.5 MB (87.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
         int dp [] = new int[nums.length];
         dp[0] = nums[0];
-        dp[1] = Math.max(nums[1], nums[0]);
         
-        for(int i = 2; i<nums.length; i++){
+        for(int i = 1; i<nums.length; i++){
             int one = dp[i-1];
-            int two = dp[i-2] + nums[i];
+            int two;
+            if(i == 1) two = nums[i];
+            else two = dp[i-2] + nums[i];
 
             dp[i] = Math.max(one, two);
         }
