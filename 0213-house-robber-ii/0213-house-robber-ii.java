d14f8cc0c82c372c7dac67de901514002d5494fb
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-10 23:04:39 +0530

    Time: 0 ms (100%), Space: 42.7 MB (59.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,33 +1,36 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
+        if (nums.length == 1)
+            return nums[0];
 
-        if(nums.length == 2)return Math.max(nums[0], nums[1]);
+        int temp1[] = new int[nums.length - 1];
+        int temp2[] = new int[nums.length - 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i < nums.length - 1)
+                temp1[i] = nums[i];
+            if (i > 0)
+                temp2[i - 1] = nums[i];
+        }
+
+        return Math.max(getMax(temp1), getMax(temp2));
+    }
+
+    int getMax(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
 
         int dp[] = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
 
-        for (int i = 2; i < nums.length-1; i++) {
-            int pick = nums[i] + dp[i-2];
-            int skip = dp[i-1];
+        for (int i = 2; i < nums.length; i++) {
+            int pick = nums[i] + dp[i - 2];
+            int skip = dp[i - 1];
 
             dp[i] = Math.max(pick, skip);
         }
 
-        int res = dp[dp.length-2];
-
-        dp = new int[nums.length];
-        dp[1] = nums[1];
-        dp[2] = Math.max(nums[1], nums[2]);
-
-        for (int i = 3; i < nums.length; i++) {
-            int pick = nums[i] + dp[i-2];
-            int skip = dp[i-1];
-
-            dp[i] = Math.max(pick, skip);
-        }
-
-        return Math.max(res, dp[dp.length-1]);
+        return dp[dp.length - 1];
     }
 }
