5e292d42c8ab56da00e7051445ca66714e462149
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-11 22:37:26 +0530

    Time: 1 ms (96.67%), Space: 42.2 MB (96.48%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n>0 && (n&(n-1)) == 0;
+        if(n==1) return true;
+        if(n<=0 || n%2!=0) return false;
+        return isPowerOfTwo(n/2);
     }
 }
