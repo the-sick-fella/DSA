4e475d4369660c03ca56df10ec37f4f05ec7585c
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-14 21:10:10 +0530

    Time: 1 ms (16.69%), Space: 43.6 MB (44.84%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,29 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
-        int dp [][] = new int[grid.length][grid[0].length];
-        for(int row[] : dp) Arrays.fill(row, -1);
-        return recursion(grid, 0, 0, dp);
-    }
+        int m = grid.length;
+        int n = grid[0].length;
+        int dp [][] = new int[m][n];
+        dp[m-1][n-1] = grid[m-1][n-1] == 0 ? 1 : 0;
 
-    int recursion(int [][] grid, int r, int c, int dp[][]){
-        if(grid[r][c] == 1) return 0;
-        if(r == grid.length-1 && c == grid[0].length-1) return 1;   
+        for(int r = m-1; r>=0; r--){
+            for(int c = n-1; c>=0; c--){
+                if(r == m-1 && c == n-1) continue;
 
-        if(dp[r][c] != -1) return dp[r][c];     
+                if(grid[r][c] == 1){
+                    dp[r][c] = 0;
+                    continue;
+                }
 
-        int down = 0;
-        if(r < grid.length-1 && grid[r+1][c] != 1) down = recursion(grid, r+1, c, dp);
+                int down = 0;
+                if(r < grid.length-1 && grid[r+1][c] != 1) down = dp[r+1][c];
 
-        int right = 0;
-        if(c < grid[0].length-1 && grid[r][c+1] != 1) right = recursion(grid, r, c+1, dp);
+                int right = 0;
+                if(c < grid[0].length-1 && grid[r][c+1] != 1) right = dp[r][c+1];
 
-        return dp[r][c] = right + down;
+                dp[r][c] = right + down;
+            }
+        }
+
+        return dp[0][0];
     }
 }
