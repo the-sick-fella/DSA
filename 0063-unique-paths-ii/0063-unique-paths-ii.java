3a30d4e4636371f7e47a1f85a2e385841267360d
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-14 21:29:13 +0530

    Time: 1 ms (16.69%), Space: 43.6 MB (26.08%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,33 +2,36 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int dp [][] = new int[m][n];
+        int dp [] = new int[n];
 
         int prev = 0;
         for(int c = n-1; c>=0; c--){
-            if(c < n-1 && dp[m-1][c+1] == 0) prev = 1;
+            if(c < n-1 && dp[c+1] == 0) prev = 1;
             if(prev != 1 && grid[m-1][c] != 1){
-                dp[m-1][c] = 1;
+                dp[c] = 1;
             }
         }
 
         for(int r = m-2; r>=0; r--){
+            int curr[] = new int[n];
             for(int c = n-1; c>=0; c--){
                 if(grid[r][c] == 1){
-                    dp[r][c] = 0;
+                    curr[c] = 0;
                     continue;
                 }
 
                 int down = 0;
-                if(r < grid.length-1 && grid[r+1][c] != 1) down = dp[r+1][c];
+                if(r < grid.length-1 && grid[r+1][c] != 1) down = dp[c];
 
                 int right = 0;
-                if(c < grid[0].length-1 && grid[r][c+1] != 1) right = dp[r][c+1];
+                if(c < grid[0].length-1 && grid[r][c+1] != 1) right = curr[c+1];
 
-                dp[r][c] = right + down;
+                curr[c] = right + down;
             }
+
+            dp = curr;
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 }
