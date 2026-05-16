8027e66616fa2bd71b301f5686016278f8e09545
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-16 20:59:33 +0530

    Time: 23 ms (81.89%), Space: 46 MB (94.06%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int dp[][][] = new int[m][n][n];
+        for (int row[][] : dp) {
+            for (int col[] : row) {
+                Arrays.fill(col, -1);
+            }
+        }
+        return f(grid, m, n, 0, 0, n - 1, dp);
+    }
+
+    int f(int[][] grid, int m, int n, int r, int c1, int c2, int[][][] dp) {
+        if (r >= m || c1 < 0 || c1 >= n || c2 < 0 || c2 >= n) return 0;
+
+        if (dp[r][c1][c2] != -1) return dp[r][c1][c2];
+
+        int res = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                res = Math.max(res, f(grid, m, n, r + 1, c1 + i, c2 + j, dp));
+            }
+        }
+
+        res += grid[r][c1];
+
+        if (c1 == c2) return dp[r][c1][c2] = res;
+
+        return dp[r][c1][c2] = res + grid[r][c2];
+    }
+}
