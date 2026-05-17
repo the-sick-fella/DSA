a0f14668c42f11807185ff2e0087df04e96671b4
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-17 17:53:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -2,51 +2,24 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        return f(grid, m, n, 0, 0, n - 1);
+    }
 
-        int dp[][] = new int[n][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, (int) -1e9);
-        }
+    int f(int[][] grid, int m, int n, int r, int c1, int c2) {
+        if (r >= m || c1 < 0 || c1 >= n || c2 < 0 || c2 >= n)
+            return 0;
 
-        dp[0][n - 1] = grid[0][0] + grid[0][n - 1];
-
-        for (int r = 1; r < m; r++) {
-            int curr[][] = new int[n][n];
-            for (int c1 = 0; c1 < n; c1++) {
-                for (int c2 = 0; c2 < n; c2++) {
-                    int res = (int) -1e9;
-                    for (int i = -1; i <= 1; i++) {
-                        if (c1 + i < 0 || c1 + i >= n)
-                            continue;
-
-                        for (int j = -1; j <= 1; j++) {
-                            if (c2 + j < 0 || c2 + j >= n)
-                                continue;
-
-                            res = Math.max(res, dp[c1 + i][c2 + j]);
-                        }
-                    }
-
-                    res += grid[r][c1];
-
-                    if (c1 == c2) {
-                        curr[c1][c2] = res;
-                        continue;
-                    }
-
-                    curr[c1][c2] = res + grid[r][c2];
-                }
-            }
-
-            dp = curr;
-        }
-
-        int ans = 0;
-        for (int[] c1 : dp) {
-            for (int c2 : c1) {
-                ans = Math.max(ans, c2);
+        int res = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                res = Math.max(res, f(grid, m, n, r + 1, c1 + i, c2 + j));
             }
         }
-        return ans;
+
+        res += grid[r][c1];
+
+        if (c1 == c2)
+            return res;
+        return res += grid[r][c2];
     }
 }
