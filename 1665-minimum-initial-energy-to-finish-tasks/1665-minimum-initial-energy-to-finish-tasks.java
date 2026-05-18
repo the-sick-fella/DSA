87c347e4f49328b5756b7b02a2aec2a2a28f2818
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-19 00:20:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -1,19 +1,5 @@
 class Solution {
     public int minimumEffort(int[][] tasks) {
-        Arrays.sort(tasks, (a,b) -> b[1] - b[0] - (a[1] - a[0]));
-
-        int curr = 0;
-        int ans = 0;
-
-        for(int task[] : tasks){
-            if(task[1] > curr){
-                ans += (task[1] - curr);
-                curr = task[1];
-            }
-
-            curr -= task[0];
-        }
-
-        return ans;
+        
     }
 }
