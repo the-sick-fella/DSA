8db386739654ebffa006a4b038fd7824f6eefab0
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-18 21:02:54 +0530

    Time: 18 ms (94.72%), Space: 46.2 MB (88.04%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -3,18 +3,19 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        int dp[][][] = new int[m][n][n];
-        for (int[][] mat : dp) {
-            for (int[] row : mat) {
-                Arrays.fill(row, (int)-1e9);
-            }
+        int dp[][] = new int[n][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, (int) -1e9);
         }
-        dp[0][0][n - 1] = grid[0][0] + grid[0][n - 1];
+
+        dp[0][n - 1] = grid[0][0] + grid[0][n - 1];
 
         for (int r = 1; r < m; r++) {
+            int curr[][] = new int[n][n];
+
             for (int c1 = 0; c1 < n; c1++) {
                 for (int c2 = 0; c2 < n; c2++) {
-                    int res = (int)-1e9;
+                    int res = (int) -1e9;
                     
                     for (int i = -1; i <= 1; i++) {
                         if (c1 + i < 0 || c1 + i >= n)
@@ -24,24 +25,26 @@ class Solution {
                             if (c2 + j < 0 || c2 + j >= n)
                                 continue;
 
-                            res = Math.max(res, dp[r - 1][c1 + i][c2 + j]);
+                            res = Math.max(res, dp[c1 + i][c2 + j]);
                         }
                     }
 
                     res += grid[r][c1];
 
                     if (c1 == c2) {
-                        dp[r][c1][c2] = res;
+                        curr[c1][c2] = res;
                         continue;
                     }
 
-                    dp[r][c1][c2] = res + grid[r][c2];
+                    curr[c1][c2] = res + grid[r][c2];
                 }
             }
+
+            dp = curr;
         }
 
         int ans = 0;
-        for (int c1[] : dp[m - 1]) {
+        for (int[] c1 : dp) {
             for (int c2 : c1) {
                 ans = Math.max(ans, c2);
             }
