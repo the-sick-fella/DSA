64d6ac64a435319f346610915140a63edc39839f
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-21 17:58:30 +0530

    Time: 193 ms (22.29%), Space: 92.4 MB (18.2%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        HashSet<String> set = new HashSet<>();
+        for(int val : arr1){
+            String s = String.valueOf(val);
+
+            for(int i = 1; i<=s.length(); i++){
+                set.add(s.substring(0, i));
+            }
+        }
+
+        int ans = 0;
+        for(int val : arr2){
+            String s = String.valueOf(val);
+
+            for(int i = s.length(); i>0; i--){
+                if(set.contains(s.substring(0, i))){
+                    ans = Math.max(ans, i);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
