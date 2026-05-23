11b66a0905dd69d227a18b5141aa406d1ac9101b
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-23 22:13:39 +0530

    Time: 163 ms (35.82%), Space: 44.9 MB (84.69%) - LeetHub

--- 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.java
+++ 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.java
@@ -1,23 +1,17 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        if(nums.length == 1) return 0;
         int ans = Integer.MAX_VALUE;
-        for(int x = 0; x<k; x++){
-            for(int y = 0; y<k; y++){
-                if(x == y) continue;
+        for (int x = 0; x < k; x++) {
+            for (int y = 0; y < k; y++) {
+                if (x == y) continue;
                 int count = 0;
 
-                for(int i = 0; i<nums.length; i++){
+                for (int i = 0; i < nums.length; i++) {
                     int mod = nums[i] % k;
-                    
-                    if(i%2 == 0){
-                        int diff = Math.abs(mod - x);
-                        count += Math.min(diff, k-diff);
-                    }
-                    else{
-                        int diff = Math.abs(mod - y);
-                        count += Math.min(diff, k-diff);
-                    }
+                    int target = i % 2 == 0 ? x : y;
+
+                    int diff = Math.abs(mod - target);
+                    count += Math.min(diff, k - diff);
                 }
 
                 ans = Math.min(ans, count);
