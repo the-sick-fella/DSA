c08f59646db77c97724061ffb2134d8bf0d1e733
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-29 14:34:22 +0530

    Time: 0 ms (100%), Space: 43.4 MB (65.1%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        char[][] mat = new char[8][4];
+        char c = 'a';
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                mat[i][j] = c++;
+            }
+
+            if (i == 5 || i == 7) {
+                mat[i][3] = c++;
+            }
+        }
+
+        List<String> list = new ArrayList<>();
+
+        f(mat, digits, 0, list, new StringBuilder());
+        return list;
+    }
+
+    void f(char[][] mat, String s, int idx, List<String> list, StringBuilder sb) {
+        if (idx >= s.length()) {
+            list.add(sb.toString());
+            return;
+        }
+
+        char c = s.charAt(idx);
+        int r = c - '2';
+
+        sb.append(mat[r][0]);
+        f(mat, s, idx + 1, list, sb);
+        sb.deleteCharAt(sb.length() - 1);
+
+        sb.append(mat[r][1]);
+        f(mat, s, idx + 1, list, sb);
+        sb.deleteCharAt(sb.length() - 1);
+
+        sb.append(mat[r][2]);
+        f(mat, s, idx + 1, list, sb);
+        sb.deleteCharAt(sb.length() - 1);
+
+        if (r == 5 || r == 7) {
+            sb.append(mat[r][3]);
+            f(mat, s, idx + 1, list, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
