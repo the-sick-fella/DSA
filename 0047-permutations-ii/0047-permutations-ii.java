474649ce578214a80de3749bde8ff3e322851b19
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-05-30 19:30:31 +0530

    Time: 5 ms (18.43%), Space: 46.5 MB (88%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Set<List<Integer>> set = new HashSet<>();
-        f(nums, set, new ArrayList<>(), new ArrayList<>());
         List<List<Integer>> list = new ArrayList<>();
-        for(List<Integer> curr : set) list.add(new ArrayList<>(curr));
+        Arrays.sort(nums);
+        f(nums, list, new ArrayList<>(), new ArrayList<>());
         return list;
     }
 
-    void f(int [] nums, Set<List<Integer>> set, List<Integer> curr, List<Integer> visited){
+    void f(int [] nums, List<List<Integer>> list, List<Integer> curr, List<Integer> visited){ 
         if(curr.size() == nums.length){
-            set.add(new ArrayList<>(curr));
+            list.add(new ArrayList<>(curr));
             return;
         }
 
         for(int i = 0; i<nums.length; i++){
             if(visited.contains(i)) continue;
+            if(i>0 && !visited.contains(i-1) && nums[i] == nums[i-1]) continue;
 
             curr.add(nums[i]);
             visited.add(i);
-            f(nums, set, curr, visited);
+            f(nums, list, curr, visited);
             curr.remove(curr.size()-1);
             visited.remove(visited.size()-1);
         }
