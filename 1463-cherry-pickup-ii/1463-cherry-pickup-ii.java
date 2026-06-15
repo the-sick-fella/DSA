b0478f0a600c6c6adf0a5bb4d9e56fe3aeacd089
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-06-15 22:27:48 +0530

    Time: 20 ms (94.64%), Space: 46.4 MB (45.28%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -2,26 +2,37 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int dp [][][] = new int [m][n][n];
-        for(int r[][] : dp){
-            for(int r2[] : r) Arrays.fill(r2, -1);
+        int dp[][][] = new int[m][n][n];
+        for(int mat[][]:dp){
+            for(int r[]:mat) Arrays.fill(r, (int)-1e9);
         }
-        return f(grid, m, n, 0, 0, n-1, dp);
-    }
+        dp[0][0][n - 1] = grid[0][0] + grid[0][n - 1];
+        for (int r = 1; r < m; r++) {
+            for (int c1 = 0; c1 < n; c1++) {
+                for (int c2 = 0; c2 < n; c2++) {
+                    int max = (int)-1e9;
+                    for (int i = -1; i < 2; i++) {
+                        if (c1 + i < 0 || c1 + i >= n) continue;
+                        for (int j = -1; j < 2; j++) {
+                            if (c2 + j < 0 || c2 + j >= n) continue;
+                            max = Math.max(max, dp[r - 1][c1 + i][c2 + j]);
+                        }
+                    }
 
-    int f(int [][]grid, int m, int n, int r, int c1, int c2, int dp[][][]){
-        if(r<0 || r >= m || c1<0 || c2<0 || c1 >= n || c2 >=n) return 0;
-
-        if(dp[r][c1][c2] != -1) return dp[r][c1][c2];
-
-        int max = 0;
-        for(int i = -1; i<2; i++){
-            for(int j = -1; j<2; j++){
-                max = Math.max(max, f(grid, m, n, r+1, c1+i, c2+j,dp));
+                    dp[r][c1][c2] = max + grid[r][c1];
+                    if (c1 != c2)
+                        dp[r][c1][c2] += grid[r][c2];
+                }
             }
         }
 
-        if(c1 == c2) return dp[r][c1][c2] = max + grid[r][c1];
-        return dp[r][c1][c2] = max + grid[r][c1] + grid[r][c2];
+        int ans = 0;
+
+        for (int r[] : dp[m - 1]) {
+            for (int val : r)
+                ans = Math.max(ans, val);
+        }
+
+        return ans;
     }
 }
