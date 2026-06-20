d37baf735d7a4ddcfd18d8b8204c8d3db52e30fc
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-06-20 14:46:15 +0530

    Time: 33 ms (68.5%), Space: 72.4 MB (35.03%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,19 +6,20 @@ class Solution {
 
         if(sum%2 != 0) return false;
 
-        int [][] dp = new int[n][sum+1];
+        int target = sum/2;
+        int [][] dp = new int[n][target+1];
         for(int r[] : dp) Arrays.fill(r, -1);
 
-        return f(nums, n, 0, 0, sum, dp) == 1 ? true : false;
+        return f(nums, n, 0, 0, target, dp) == 1 ? true : false;
     }
 
-    int f(int [] nums, int n, int idx, int curr, int sum, int [][] dp){
-        if(curr == sum/2) return 1;
-        if(idx >= n || curr > sum/2) return 0;
+    int f(int [] nums, int n, int idx, int curr, int target, int [][] dp){
+        if(curr == target) return 1;
+        if(idx >= n || curr > target) return 0;
 
         if(dp[idx][curr] != -1) return dp[idx][curr];
 
-        if(f(nums, n, idx+1, curr+nums[idx], sum, dp) == 1 || f(nums, n, idx+1, curr, sum, dp) == 1) return dp[idx][curr] = 1;
+        if(f(nums, n, idx+1, curr+nums[idx], target, dp) == 1 || f(nums, n, idx+1, curr, target, dp) == 1) return dp[idx][curr] = 1;
         return dp[idx][curr] = 0;
     }
 }
