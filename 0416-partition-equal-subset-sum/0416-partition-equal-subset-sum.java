f1665df75ec2a987106b0cff572abee5c9d68cbd
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-06-20 14:47:20 +0530

    Time: 29 ms (84.12%), Space: 72.7 MB (25.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
         int sum = 0;
         for(int num : nums) sum += num;
 
         if(sum%2 != 0) return false;
 
         int target = sum/2;
-        int [][] dp = new int[n][target+1];
+        int [][] dp = new int[nums.length][target+1];
         for(int r[] : dp) Arrays.fill(r, -1);
 
-        return f(nums, n, 0, 0, target, dp) == 1 ? true : false;
+        return f(nums, 0, 0, target, dp) == 1 ? true : false;
     }
 
-    int f(int [] nums, int n, int idx, int curr, int target, int [][] dp){
+    int f(int [] nums, int idx, int curr, int target, int [][] dp){
         if(curr == target) return 1;
-        if(idx >= n || curr > target) return 0;
+        if(idx >= nums.length || curr > target) return 0;
 
         if(dp[idx][curr] != -1) return dp[idx][curr];
 
-        if(f(nums, n, idx+1, curr+nums[idx], target, dp) == 1 || f(nums, n, idx+1, curr, target, dp) == 1) return dp[idx][curr] = 1;
+        if(f(nums, idx+1, curr+nums[idx], target, dp) == 1 || f(nums, idx+1, curr, target, dp) == 1) return dp[idx][curr] = 1;
         return dp[idx][curr] = 0;
     }
 }
