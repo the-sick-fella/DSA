9f708ef2e76ecb009dd57ee1401b7760e2996fe8
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-06-21 21:09:57 +0530

    Time: 47 ms (5.69%), Space: 48.3 MB (12.66%) - LeetHub

---
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int maxDistance(String moves) {
+        Map<Character, Integer> map = new HashMap<>();
+        int blank = 0;
+
+        for(char c : moves.toCharArray()){
+            switch(c){
+                case '_': blank++;
+                    break;
+                case 'U': map.put('U', map.getOrDefault('U', 0) + 1 );
+                    break;
+                case 'D': map.put('D', map.getOrDefault('D', 0) + 1 );
+                    break;
+                case 'L': map.put('L', map.getOrDefault('L', 0) + 1 );
+                    break;
+                case 'R': map.put('R', map.getOrDefault('R', 0) + 1 );
+                    break;
+            }
+        }
+
+        // int max = 0;
+        // char maxChar = ' ';
+
+        // for(char c : map.keySet()){
+        //     if(Math.abs(map.get(c)) >= max){
+        //         maxChar = c;
+        //         max = Math.abs(map.get(c));
+        //     }
+        // }
+
+
+        int x = 0, y = 0;
+        // map.put(maxChar, max+=blank);
+
+        for(char c : map.keySet()){
+            switch(c){
+                case 'U': y+=map.get('U');
+                    break;
+                case 'D': y-=map.get('D');
+                    break;
+                case 'L': x-=map.get('L');
+                    break;
+                case 'R': x+=map.get('R');
+                    break;
+            }
+        }
+
+        return Math.abs(x) + Math.abs(y) + blank;
+    }
+}
