5dd7ebbb969bd7775b73972a06f0e45e9ca47b9e
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-06-28 10:07:45 +0530

    Time: 82 ms (10.52%), Space: 155.5 MB (11.6%) - LeetHub

--- 3975-filter-occupied-intervals/3975-filter-occupied-intervals.java
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.java
@@ -1,33 +1,28 @@
 class Solution {
     public List<List<Integer>> filterOccupiedIntervals(int[][] nums, int freeStart, int freeEnd) {
-        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums, (a,b) -> a[0] - b[0]);
+        List<List<Integer>> list = new ArrayList<>();
         int n = nums.length;
-        List<Integer> temp = new ArrayList<>();
-        temp.add(nums[0][0]);
-        temp.add(nums[0][1]);
-        list.add(temp);
+        int first = nums[0][0];
+        int sec = nums[0][1];
+        list.add(List.of(first, sec));
         for(int i = 1; i<n; i++){
-            temp = list.get(list.size()-1);
-                int first = temp.get(0);
-                int sec = temp.get(1);
-                temp = new ArrayList<>();
+            List<Integer>temp = list.get(list.size()-1);
+            first = temp.get(0);
+            sec = temp.get(1);
             if(sec >= nums[i][0]-1){
                 sec = Math.max(sec, nums[i][1]);
-                temp.add(first);
-                temp.add(sec);
                 list.remove(list.size()-1);
+                list.add(List.of(first, sec));
             } else{
-                temp.add(nums[i][0]);
-                temp.add(nums[i][1]);
+                list.add(List.of(nums[i][0], nums[i][1]));
             }
-            list.add(List.copyOf(temp));
         }
         
         List<List<Integer>> ans = new ArrayList<>();
         for(List<Integer> curr : list){
-            int first = curr.get(0);
-            int sec = curr.get(1);
+            first = curr.get(0);
+            sec = curr.get(1);
             if(first > freeEnd || sec < freeStart){
                 ans.add(curr);
                 continue;
