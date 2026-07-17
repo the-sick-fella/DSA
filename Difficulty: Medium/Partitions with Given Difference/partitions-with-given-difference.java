c95c5640f327f471ee69a31121187a02982d8832
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-17 20:20:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
@@ -2,32 +2,26 @@ class Solution {
 	public int countPartitions(int[] arr, int diff) {
 		// code here
 		int sum = 0;
-		for (int num : arr)
-			sum += num;
+		for (int num : arr) sum += num;
 		
 		int target = sum - diff;
-		if (target<0 || target%2 != 0)
-			return 0;
+		if (target < 0 || target%2 != 0) return 0;
 		target /= 2;
 		
 		int dp [][] = new int[arr.length + 1][target + 1];
-		for (int r[] : dp)
-			Arrays.fill(r, -1);
+		for (int r[] : dp) Arrays.fill(r, -1);
 		
 		return f(arr, 0, 0, target, dp);
 	}
 	
 	static int f(int [] nums, int idx, int curr, int target, int [][] dp) {
-		if (curr > target)
-			return 0;
+		if (curr > target) return 0;
 		if (idx == nums.length) {
-			if (curr == target)
-				return 1;
+			if (curr == target) return 1;
 			return 0;
 		}
 		
-		if (dp[idx][curr] != -1)
-			return dp[idx][curr];
+		if (dp[idx][curr] != -1) return dp[idx][curr];
 		
 		int pick = f(nums, idx + 1, curr + nums[idx], target, dp);
 		int skip = f(nums, idx + 1, curr, target, dp);
