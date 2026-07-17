28cbc2d72ec9254582ffe29a3e441e5618bbec40
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-17 20:28:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
@@ -8,21 +8,23 @@ class Solution {
 		if (target < 0 || target%2 != 0) return 0;
 		target /= 2;
 		
-		int dp [][] = new int[arr.length][target + 1];
+		int dp [] = new int[target + 1];
 
-		dp[0][0]++;
-		if (arr[0] <= target) dp[0][arr[0]]++;
+		dp[0]++;
+		if (arr[0] <= target) dp[arr[0]]++;
 		
 		for (int idx = 1; idx<arr.length; idx++) {
+		    int [] temp = new int[target+1];
 			for (int curr = 0; curr <= target; curr++) {
-				int skip = dp[idx - 1][curr];
+				int skip = dp[curr];
 				int pick = 0;
-				if (arr[idx] <= curr) pick = dp[idx - 1][curr - arr[idx]];
+				if (arr[idx] <= curr) pick = dp[curr - arr[idx]];
 				
-				dp[idx][curr] = skip + pick;
+				temp[curr] = skip + pick;
 			}
+			dp = temp;
 		}
 		
-		return dp[arr.length-1][target];
+		return dp[target];
 	}
 }
