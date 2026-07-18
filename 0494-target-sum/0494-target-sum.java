9e7f69c066219b3af9e6a3d996c761f8d8493b53
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-19 01:34:48 +0530

    Time: 12 ms (50.08%), Space: 46.9 MB (18.97%) - LeetHub - Time: O(n * sum), Space: O(n * sum)

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+
+        if (target > sum || target < 0 - sum) return 0;
+
+        int[][] dp = new int[nums.length][2 * sum + 1];
+        dp[0][sum + nums[0]]++;
+        dp[0][sum - nums[0]]++;
+
+        for (int idx = 1; idx < nums.length; idx++) {
+            for (int curr = 0 - sum; curr <= sum; curr++) {
+                int add = 0, sub = 0;
+                if (sum + curr - nums[idx] >= 0)  add = dp[idx - 1][sum + curr - nums[idx]];
+                if (curr + nums[idx] <= sum) sub = dp[idx - 1][sum + curr + nums[idx]];
+                dp[idx][sum + curr] = add + sub;
+            }
+        }
+        return dp[nums.length - 1][sum + target];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
