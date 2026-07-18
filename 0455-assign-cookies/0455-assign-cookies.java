ef17d4e8f5df5d7892b7f859524b3f2cf197964e
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-19 01:41:31 +0530

    Time: 14 ms (92.24%), Space: 51.5 MB (56.46%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(s);
+        Arrays.sort(g);
+
+        int i = 0, j = 0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
