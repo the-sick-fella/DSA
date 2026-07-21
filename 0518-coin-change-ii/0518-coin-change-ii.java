c6838ac66898b98a0a53a62786a0f77d28acc012
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-21 22:09:50 +0530

    Time: 9 ms (67.18%), Space: 42.9 MB (83.78%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,11 +2,9 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length; 
         int [] dp = new int [amount+1];
-        for(int sum = 0; sum<=amount; sum++){
-            if(sum%coins[0] == 0) dp[sum] = 1;
-        }
+        dp[0] = 1;
 
-        for(int idx = 1; idx<n; idx++){
+        for(int idx = 0; idx<n; idx++){
             for(int sum = 0; sum<=amount; sum++){
                 int skip = dp[sum];
                 int pick = 0;
