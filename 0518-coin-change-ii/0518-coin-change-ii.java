d8875dd7048f3f00e892cf7a6d869ba58e8416bb
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-21 21:58:22 +0530

    Time: 11 ms (60.35%), Space: 57.4 MB (11.29%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,20 +1,21 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length; 
-        int [][] dp = new int [n][amount+1];
-        for(int sum = 0; sum<=amount; sum++){
-            if(sum%coins[0] == 0) dp[0][sum] = 1;
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for (int sum = 0; sum <= amount; sum++) {
+            if (sum % coins[0] == 0)
+                dp[0][sum] = 1;
         }
 
-        for(int idx = 1; idx<n; idx++){
-            for(int sum = 0; sum<=amount; sum++){
-                int skip = dp[idx-1][sum];
+        for (int idx = 1; idx < n; idx++) {
+            for (int sum = 0; sum <= amount; sum++) {
+                int skip = dp[idx - 1][sum];
                 int pick = 0;
-                if(sum >= coins[idx]) pick = dp[idx][sum - coins[idx]];
+                if (sum >= coins[idx])
+                    pick = dp[idx][sum - coins[idx]];
                 dp[idx][sum] = skip + pick;
             }
         }
-        int ans = dp[n-1][amount];
-        return ans > 0 ? ans : 0;
+        return dp[n - 1][amount];
     }
 }
