675c635506e5c12b311d0a7414a6432f69cd25a2
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-21 21:05:50 +0530

    Time: 3 ms (29.8%), Space: 44.4 MB (14.05%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -6,16 +6,21 @@ class Solution {
             map.putIfAbsent(c, i);
         }
         StringBuilder sb = new StringBuilder();
+        Set<Character> set = new HashSet<>();
         sb.append(s.charAt(0));
+        set.add(s.charAt(0));
         for (int i = 1; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (sb.indexOf(String.valueOf(c)) != -1)
+            if (set.contains(c))
                 continue;
+
             while (sb.length() > 0 && c < sb.charAt(sb.length() - 1) && map.get(sb.charAt(sb.length() - 1)) > i) {
+                set.remove(sb.charAt(sb.length() - 1));
                 sb.deleteCharAt(sb.length() - 1);
             }
 
             sb.append(c);
+            set.add(c);
         }
         return sb.toString();
     }
