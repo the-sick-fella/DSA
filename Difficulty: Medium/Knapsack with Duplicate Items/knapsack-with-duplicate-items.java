2012330047aee0db4725bce01fc1f314562a752c
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-22 20:56:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -2,8 +2,24 @@ class Solution {
     public int knapSack(int val[], int wt[], int capacity) {
         // code here
         int dp [][] = new int[wt.length][capacity+1];
-        for(int r[]:dp) Arrays.fill(r, -1);
-        return f(val, wt, capacity, 0, 0, dp);
+        // for(int sum = 0; sum<=capacity; sum++){
+        //     if(sum%wt[0] == 0) dp[0][sum] = sum/wt[0] * val[0];
+        // }
+        dp[0][0] = 0;
+        
+        // for(int num : dp[0]) System.out.println(num);
+        
+        for(int idx = 0; idx < wt.length; idx++){
+            for(int sum = 0; sum <= capacity; sum++){
+                int skip = 0;
+                if(idx > 0) skip = dp[idx-1][sum];
+                int pick = 0;
+                if(sum >= wt[idx]) pick = dp[idx][sum - wt[idx]] + val[idx];
+                
+                dp[idx][sum] = Math.max(skip, pick);
+            }
+        }
+        return dp[wt.length-1][capacity];
     }
     
     static int f(int [] val, int [] wt, int cap, int idx, int sum, int [][] dp){
