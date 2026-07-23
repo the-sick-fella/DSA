cf8301d614fa6636c7e6061ae7b729e42c66c517
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-23 21:54:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.java
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -2,19 +2,21 @@ class Solution {
 	public int cutRod(int[] price) {
 		// code here
 		int n = price.length;
-		int [][] dp = new int[n][n + 1];
+		int [] dp = new int[n + 1];
 		for (int idx = 0; idx<n; idx++) {
+		    int temp [] = new int[n+1];
 			for (int sum = 0; sum <= n; sum++) {
 				int skip = 0;
-				if (idx>0) skip = dp[idx - 1][sum];
+				if (idx>0) skip = dp[sum];
 				
 				int pick = 0;
-				if (sum - idx - 1 >= 0) pick = dp[idx][sum - idx - 1] + price[idx];
+				if (sum - idx - 1 >= 0) pick = temp[sum - idx - 1] + price[idx];
 				
-				dp[idx][sum] = Math.max(skip, pick);
+				temp[sum] = Math.max(skip, pick);
 			}
+			dp=temp;
 		}
 		
-		return dp[n - 1][n];
+		return dp[n];
 	}
 }
