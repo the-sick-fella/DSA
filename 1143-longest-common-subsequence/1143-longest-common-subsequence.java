dde65279154e9d2bcdf9db4baedfaee42eef07a2
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-24 22:37:44 +0530

    Time: 125 ms (5.4%), Space: 54.8 MB (5.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,20 +5,20 @@ class Solution {
         for (int r[] : dp)
             Arrays.fill(r, -1);
 
-        return f(s1, s2, 0, 0, dp);
+        return f(s1, m, s2, n, 0, 0, dp);
     }
 
-    int f(String s1, String s2, int i1, int i2, int [][] dp){
-        if(i1 >= s1.length() || i2 >= s2.length()) return 0;
+    int f(String s1, int m, String s2, int n, int i1, int i2, int [][] dp){
+        if(i1 >= m || i2 >= n) return 0;
         if(dp[i1][i2] != -1) return dp[i1][i2];
 
         int pick = 0;
         int temp = s2.substring(i2, s2.length()).indexOf(s1.charAt(i1));
         if(temp != -1){
-            pick = 1 + f(s1, s2, i1+1, temp+i2+1, dp);
+            pick = 1 + f(s1, m, s2, n, i1+1, temp+i2+1, dp);
         } 
 
-        int skip = f(s1, s2, i1+1, i2, dp);
+        int skip = f(s1, m, s2, n, i1+1, i2, dp);
 
         return dp[i1][i2] = Math.max(skip, pick);
     }
