95742568e8de58f838e2dc546ffe0f915ee3ca34
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-07-29 21:22:50 +0530

    Time: 533 ms (5.41%), Space: 59.6 MB (5.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,25 +1,22 @@
 class Solution {
     public int longestCommonSubsequence(String s1, String s2) {
         int m = s1.length(), n = s2.length();
-        int dp[][] = new int[m][n];
-        for (int r[] : dp)
-            Arrays.fill(r, -1);
+        int dp[][] = new int[m+1][n+1];
 
-        return f(s1, m, s2, n, 0, 0, dp);
-    }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int pick = 0;
+                int temp = s2.substring(j, s2.length()).indexOf(s1.charAt(i));
+                if (temp != -1) {
+                    pick = 1 + dp[i + 1][temp + j + 1];
+                }
 
-    int f(String s1, int m, String s2, int n, int i1, int i2, int [][] dp){
-        if(i1 >= m || i2 >= n) return 0;
-        if(dp[i1][i2] != -1) return dp[i1][i2];
+                int skip = dp[i + 1][j];
 
-        int pick = 0;
-        int temp = s2.substring(i2, s2.length()).indexOf(s1.charAt(i1));
-        if(temp != -1){
-            pick = 1 + f(s1, m, s2, n, i1+1, temp+i2+1, dp);
-        } 
+                dp[i][j] = Math.max(skip, pick);
+            }
+        }
 
-        int skip = f(s1, m, s2, n, i1+1, i2, dp);
-
-        return dp[i1][i2] = Math.max(skip, pick);
+        return dp[0][0];
     }
 }
