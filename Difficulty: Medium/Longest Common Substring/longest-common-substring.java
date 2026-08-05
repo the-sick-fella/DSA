86792687d799d4487439b01b71bcac8fcd7d051c
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-05 20:14:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -2,14 +2,16 @@ class Solution {
 	public int longCommSubstr(String s1, String s2) {
 		// code here
 		int m = s1.length(), n = s2.length();
-		int dp[][] = new int [m + 1][n + 1];
+		int dp[] = new int [n + 1];
 		int max = 0;
 		for (int i = 1; i <= m; i++) {
+		    int temp [] = new int[n+1];
 			for (int j = 1; j <= n; j++) {
-				if (s1.charAt(i - 1) == s2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
+				if (s1.charAt(i - 1) == s2.charAt(j - 1)) temp[j] = dp[j - 1] + 1;
 					
-				max = Math.max(dp[i][j], max);
+				max = Math.max(temp[j], max);
 			}
+			dp = temp;
 		}
 		
 		return max;
