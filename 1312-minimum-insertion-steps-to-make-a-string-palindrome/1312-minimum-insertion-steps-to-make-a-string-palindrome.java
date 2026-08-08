85342ac1d96106a8a83effba6a5e14497992ee7a
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-08 18:23:45 +0530

    Time: 19 ms (85.82%), Space: 47.4 MB (50.63%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,17 +1,15 @@
 class Solution {
     public int minInsertions(String s) {
         int n = s.length();
-        int[] dp = new int[n + 1];
-        for (int i = n - 1; i >= 0; i--) {
-            int temp[] = new int[n+1];
-            for (int j = i + 1; j <= n; j++) {
-                if (s.charAt(i) == s.charAt(j-1))
-                    temp[j] = dp[j - 1];
-                else
-                    temp[j] = 1 + Math.min(dp[j], temp[j - 1]);
-            }
-            dp = temp;
-        }
-        return dp[n];
+        int [][] dp = new int[n+1][n+1];
+        for(int r[]:dp) Arrays.fill(r, -1);
+        return f(s, 0, n-1, dp);
+    }
+
+    int f(String s, int i, int j, int [][]dp){
+        if(i>=j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i) == s.charAt(j)) return dp[i][j] = f(s, i+1, j-1, dp);
+        return dp[i][j] = 1+Math.min(f(s, i+1, j, dp), f(s, i, j-1, dp));
     }
 }
