4d8e5be091ae5305c7706e8385f48372b34891e8
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-10 20:17:47 +0530

    Time: 22 ms (24.21%), Space: 54 MB (78.31%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,24 +1,14 @@
 class Solution {
     public int numDistinct(String s, String t) {
         int m = s.length(), n = t.length();
-        int[][] dp = new int[m][n];
-        for (int r[] : dp)
-            Arrays.fill(r, -1);
-        return f(s, t, 0, 0, dp);
-    }
-
-    int f(String s, String t, int i, int j, int[][] dp) {
-        if (j == t.length())
-            return 1;
-        if (i == s.length())
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-            
-        dp[i][j] = f(s, t, i + 1, j, dp);
-        if (s.charAt(i) == t.charAt(j))
-            dp[i][j] += f(s, t, i + 1, j + 1, dp);
-
-        return dp[i][j];
+        int[][] dp = new int[m+1][n+1];
+        for (int r[] : dp)r[n]=1;
+        for(int i = m-1; i>=0; i--){
+            for(int j = n-1; j>=0; j--){
+                dp[i][j] = dp[i+1][j];
+                if(s.charAt(i) == t.charAt(j)) dp[i][j] += dp[i+1][j+1];
+            }
+        }
+        return dp[0][0];
     }
 }
