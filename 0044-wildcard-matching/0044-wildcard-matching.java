09bb4f9228917ce10b33d7b8d4537fe5e5b313f6
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-12 21:36:44 +0530

    Time: 47 ms (35.08%), Space: 47.1 MB (68.45%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,25 +1,27 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         int m = s.length(), n = p.length();
-        boolean[][] dp = new boolean[m+1][n+1];
-        dp[m][n] = true;
+        boolean[] dp = new boolean[n + 1];
+        dp[n] = true;
         boolean flag = true;
         for (int j = n - 1; j >= 0; j--) {
             if (p.charAt(j) != '*')
                 flag = false;
-            dp[m][j] = flag;
+            dp[j] = flag;
         }
 
         for (int i = m - 1; i >= 0; i--) {
+            boolean temp[] = new boolean[n + 1];
             for (int j = n - 1; j >= 0; j--) {
                 if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j))
-                    dp[i][j] = dp[i + 1][j + 1];
+                    temp[j] = dp[j + 1];
                 else if (p.charAt(j) == '*') {
-                    if (dp[i + 1][j] || dp[i][j + 1])
-                        dp[i][j] = true;
+                    if (dp[j] || temp[j + 1])
+                        temp[j] = true;
                 }
             }
+            dp = temp;
         }
-        return dp[0][0];
+        return dp[0];
     }
 }
