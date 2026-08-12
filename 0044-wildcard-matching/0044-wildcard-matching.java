c790a2796336ec6054e67480f736b08818ef35de
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-12 21:35:07 +0530

    Time: 54 ms (25.12%), Space: 47.6 MB (41.08%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -22,27 +22,4 @@ class Solution {
         }
         return dp[0][0];
     }
-
-    int f(String s, String p, int i, int j, int[][] dp) {
-        // if(i == s.length() && j == p.length()) return 1;
-        // if(j == p.length()) return 0;
-        if (i == s.length()) {
-            for (int idx = j; idx < p.length(); idx++) {
-                if (p.charAt(idx) != '*')
-                    return 0;
-            }
-            return 1;
-        }
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j))
-            return dp[i][j] = f(s, p, i + 1, j + 1, dp);
-        if (p.charAt(j) == '*') {
-            if (f(s, p, i + 1, j, dp) == 1 || f(s, p, i, j + 1, dp) == 1)
-                return 1;
-        }
-        return dp[i][j] = 0;
-    }
 }
