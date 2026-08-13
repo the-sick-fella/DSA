3933cc856cb47d0b316f27b1d68c230408166f94
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-13 21:07:06 +0530

    Time: 0 ms (100%), Space: 46.1 MB (93.33%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,43 +1,14 @@
 class Solution {
     public int maxProfit(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[2];
+        int buy = 0, sell = 0;
         for (int i = n - 1; i >= 0; i--) {
-            int temp [] = new int[2];
-            for (int bought = 0; bought < 2; bought++) {
-                int profit = 0;
-                if (bought == 1) {
-                    int sell = nums[i] + dp[0];
-                    int hold = dp[1];
-                    profit = Math.max(sell, hold);
-                } else {
-                    int buy = -nums[i] + dp[1];
-                    int skip = dp[0];
-                    profit = Math.max(buy, skip);
-                }
-                temp[bought] = profit;
-            }
-            dp = temp;
+            int currBuy = 0, currSell = 0;
+            currBuy = Math.max(nums[i] + sell, buy);
+            currSell = Math.max(-nums[i] + buy, sell);
+            buy = currBuy;
+            sell = currSell;
         }
-        return dp[0];
-    }
-
-    int f(int[] nums, int i, int bought, int[][] dp) {
-        if (i == nums.length)
-            return 0;
-        if (dp[i][bought] != -1)
-            return dp[i][bought];
-
-        int profit = 0;
-        if (bought == 1) {
-            int sell = nums[i] + f(nums, i + 1, 0, dp);
-            int hold = f(nums, i + 1, 1, dp);
-            profit = Math.max(sell, hold);
-        } else {
-            int buy = -nums[i] + f(nums, i + 1, 1, dp);
-            int skip = f(nums, i + 1, 0, dp);
-            profit = Math.max(buy, skip);
-        }
-        return dp[i][bought] = profit;
+        return sell;
     }
 }
