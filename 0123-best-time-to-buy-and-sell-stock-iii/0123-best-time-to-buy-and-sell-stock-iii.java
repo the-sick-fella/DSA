56a946361f208bb584f6e1237a8996d0d507d651
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-13 21:58:24 +0530

    Time: 8 ms (50.53%), Space: 79.8 MB (56.62%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -7,24 +7,18 @@ class Solution {
         for (int i = n - 1; i >= 0; i--) {
             int currSell[] = new int[3];
             int currBuy[] = new int[3];
-            // for (int bought = 1; bought >= 0; bought--) {
-                for (int count = 2; count >= 0; count--) {
-                    // if (bought == 1) {
-                        int sell = nums[i] + sellDp[count];
-                        int hold = buyDp[count];
-                        currBuy[count] = Math.max(sell, hold);
-                    // } else {
-                        if (count == 2)
-                            currSell[count] = 0;
-                        else {
-                            int buy = -nums[i] + buyDp[count + 1];
-                            int skip = sellDp[count];
-                            currSell[count] = Math.max(buy, skip);
-                        }
-                    // }
+            for (int count = 2; count >= 0; count--) {
+                int sell = nums[i] + sellDp[count];
+                int hold = buyDp[count];
+                currBuy[count] = Math.max(sell, hold);
+                if (count == 2)
+                    currSell[count] = 0;
+                else {
+                    int buy = -nums[i] + buyDp[count + 1];
+                    int skip = sellDp[count];
+                    currSell[count] = Math.max(buy, skip);
                 }
-            // }
-            // dp = temp;
+            }
             buyDp = currBuy;
             sellDp = currSell;
         }
