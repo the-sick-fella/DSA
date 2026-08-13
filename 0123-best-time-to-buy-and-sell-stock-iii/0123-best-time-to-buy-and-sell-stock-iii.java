6de2caa541f75e224987bc21b64e765dca6f6db8
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-13 21:51:51 +0530

    Time: 54 ms (38.68%), Space: 117.5 MB (38.25%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,26 +1,46 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int [][][] dp = new int[n+1][2][3];
-        for(int mat [][] : dp){
-            for(int r[]: mat) Arrays.fill(r, -1);
+    public int maxProfit(int[] nums) {
+        int n = nums.length;
+        int[][][] dp = new int[n + 1][2][3];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int bought = 1; bought >= 0; bought--) {
+                for (int count = 2; count >= 0; count--) {
+                    if (bought == 1) {
+                        int sell = nums[i] + dp[i + 1][0][count];
+                        int hold = dp[i + 1][1][count];
+                        dp[i][bought][count] = Math.max(sell, hold);
+                    } else {
+                        if (count == 2)
+                            dp[i][bought][count] = 0;
+                        else {
+                            int buy = -nums[i] + dp[i + 1][1][count + 1];
+                            int skip = dp[i + 1][0][count];
+                            dp[i][bought][count] = Math.max(buy, skip);
+                        }
+                    }
+                }
+            }
         }
-        return f(prices, 0, 0, 0, dp);
+        return dp[0][0][0];
     }
 
-    int f(int [] nums, int i, int bought, int count, int [][][] dp){
-        if(i == nums.length) return 0;
+    int f(int[] nums, int i, int bought, int count, int[][][] dp) {
+        if (i == nums.length)
+            return 0;
 
-        if(dp[i][bought][count] != -1) return dp[i][bought][count];
+        if (dp[i][bought][count] != -1)
+            return dp[i][bought][count];
 
-        if(bought == 1){
-            int sell = nums[i] + f(nums, i+1, 0, count, dp);
-            int hold = f(nums, i+1, 1, count, dp);
+        if (bought == 1) {
+            int sell = nums[i] + f(nums, i + 1, 0, count, dp);
+            int hold = f(nums, i + 1, 1, count, dp);
             return dp[i][bought][count] = Math.max(sell, hold);
-        } else{
-            if(count == 2) return 0;
-            int buy = -nums[i] + f(nums, i+1, 1, count+1, dp);
-            int skip = f(nums, i+1, 0, count, dp);
+        } else {
+            if (count == 2)
+                return 0;
+            int buy = -nums[i] + f(nums, i + 1, 1, count + 1, dp);
+            int skip = f(nums, i + 1, 0, count, dp);
             return dp[i][bought][count] = Math.max(buy, skip);
         }
     }
