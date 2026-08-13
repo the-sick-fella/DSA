4a935f1f7efff3913a2c880260c62b801865d679
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-13 20:10:41 +0530

    Time: 2 ms (17.15%), Space: 46.7 MB (12.09%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,9 +1,21 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        for (int r[] : dp)
-            Arrays.fill(r, -1);
-        return f(prices, 0, 0, dp);
+    public int maxProfit(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n+1][2];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int bought = 0; bought < 2; bought++) {
+                if (bought == 1) {
+                    int hold = dp[i + 1][1];
+                    int sell = nums[i] + dp[i + 1][0];
+                    dp[i][bought] = Math.max(hold, sell);
+                } else {
+                    int buy = -nums[i] + dp[i + 1][1];
+                    int skip = dp[i + 1][0];
+                    dp[i][bought] = Math.max(buy, skip);
+                }
+            }
+        }
+        return dp[0][0];
     }
 
     int f(int[] nums, int i, int bought, int[][] dp) {
@@ -18,7 +30,7 @@ class Solution {
             int sell = nums[i] + f(nums, i + 1, 0, dp);
             return dp[i][bought] = Math.max(hold, sell);
         }
-        
+
         int buy = -nums[i] + f(nums, i + 1, 1, dp);
         int skip = f(nums, i + 1, 0, dp);
         return dp[i][bought] = Math.max(buy, skip);
