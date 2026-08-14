f160d9502ff53e2033c0e54bfceaeb9725887c82
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-14 20:20:25 +0530

    Time: 1 ms (69.26%), Space: 43.3 MB (89.14%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int [][] dp = new int [n+1][2];
-        for(int r[]:dp) Arrays.fill(r, -1);
-        return f(prices, 0, 0, dp);
-    }
-        
-    int f(int [] nums, int i, int b, int [][] dp){
-        if (i >= nums.length) return 0;
-        if(dp[i][b] != -1) return dp[i][b];
-
-        if(b == 1){
-            int sell = nums[i] + f(nums, i+2, 0, dp);
-            int hold = f(nums, i+1, 1, dp);
-            return dp[i][b] = Math.max(sell, hold);
-        } else{
-            int buy = -nums[i] + f(nums, i+1, 1, dp);
-            int skip = f(nums, i+1, 0, dp);
-            return dp[i][b] = Math.max(buy, skip);
+    public int maxProfit(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n + 2][2];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int b = 1; b >= 0; b--) {
+                if (b == 1) {
+                    int sell = nums[i] + dp[i + 2][0];
+                    int hold = dp[i + 1][1];
+                    dp[i][b] = Math.max(sell, hold);
+                } else {
+                    int buy = -nums[i] + dp[i + 1][1];
+                    int skip = dp[i + 1][0];
+                    dp[i][b] = Math.max(buy, skip);
+                }
+            }
         }
+        return dp[0][0];
     }
 }
