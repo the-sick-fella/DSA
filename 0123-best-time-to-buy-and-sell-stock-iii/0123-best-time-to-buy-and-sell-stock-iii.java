be9ebf2c3c558ac82a9ead31e17136e684cd80cd
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-14 20:15:02 +0530

    Time: 6 ms (52.51%), Space: 76.1 MB (59.73%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -19,22 +19,4 @@ class Solution {
         }
         return dp[0];
     }
-
-    int f(int[] nums, int i, int t, int[][] dp) {
-        if (i == nums.length || t == 4)
-            return 0;
-
-        if (dp[i][t] != -1)
-            return dp[i][t];
-
-        if (t % 2 == 1) {
-            int sell = nums[i] + f(nums, i + 1, t + 1, dp);
-            int hold = f(nums, i + 1, t, dp);
-            return dp[i][t] = Math.max(sell, hold);
-        } else {
-            int buy = -nums[i] + f(nums, i + 1, t + 1, dp);
-            int skip = f(nums, i + 1, t, dp);
-            return dp[i][t] = Math.max(buy, skip);
-        }
-    }
 }
