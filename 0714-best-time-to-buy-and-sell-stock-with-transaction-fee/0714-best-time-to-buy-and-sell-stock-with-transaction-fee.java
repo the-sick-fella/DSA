8f29d4f99f3bfa6dda4764006e78a0d4fe1c33b1
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-14 20:21:44 +0530

    Time: 16 ms (52.97%), Space: 70.5 MB (54.13%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -17,21 +17,4 @@ class Solution {
         }
         return dp[0][0];
     }
-
-    int f(int[] nums, int fee, int i, int b, int[][] dp) {
-        if (i == nums.length)
-            return 0;
-        if (dp[i][b] != -1)
-            return dp[i][b];
-
-        if (b == 1) {
-            int sell = nums[i] - fee + f(nums, fee, i + 1, 0, dp);
-            int hold = f(nums, fee, i + 1, 1, dp);
-            return dp[i][b] = Math.max(sell, hold);
-        } else {
-            int buy = -nums[i] + f(nums, fee, i + 1, 1, dp);
-            int skip = f(nums, fee, i + 1, 0, dp);
-            return dp[i][b] = Math.max(buy, skip);
-        }
-    }
 }
