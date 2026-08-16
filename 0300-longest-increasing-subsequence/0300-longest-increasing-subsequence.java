0f3144f574a0f642e46980c20cf77f0f1e22541b
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-16 18:15:11 +0530

    Time: 157 ms (19.28%), Space: 237.2 MB (14.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,31 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int dp [][] = new int [n+1][n+1];
-        for(int r [] : dp)Arrays.fill(r, -1);
+        int dp[][] = new int[n + 2][n + 2];
+        for (int i = n; i >= 1; i--) {
+            for (int prev = n - 1; prev >= 0; prev--) {
+                int skip = dp[i + 1][prev];
+                int take = 0;
+                if (prev == 0 || nums[i - 1] > nums[prev - 1])
+                    take = 1 + dp[i + 1][i];
+
+                dp[i][prev] = Math.max(skip, take);
+            }
+        }
         return f(nums, 1, 0, dp);
     }
 
-    int f(int [] nums, int i, int prev, int [][] dp){
-        if(i == nums.length+1) return 0;
+    int f(int[] nums, int i, int prev, int[][] dp) {
+        if (i == nums.length + 1)
+            return 0;
 
-        if(dp[i][prev] != -1) return dp[i][prev];
+        if (dp[i][prev] != -1)
+            return dp[i][prev];
 
-        int skip = f(nums, i+1, prev, dp);
+        int skip = f(nums, i + 1, prev, dp);
         int take = 0;
-        if(prev == 0 || nums[i-1] > nums[prev-1]) take = 1 + f(nums, i+1, i, dp);
+        if (prev == 0 || nums[i - 1] > nums[prev - 1])
+            take = 1 + f(nums, i + 1, i, dp);
 
         return dp[i][prev] = Math.max(skip, take);
     }
