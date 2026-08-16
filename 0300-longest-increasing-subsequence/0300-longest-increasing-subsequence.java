849e26ed9371fde1d2855b30f2fe9b07d65b9b50
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-16 18:18:47 +0530

    Time: 152 ms (19.4%), Space: 47 MB (25.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -16,19 +16,4 @@ class Solution {
         }
         return dp[0];
     }
-
-    int f(int[] nums, int i, int prev, int[][] dp) {
-        if (i == nums.length + 1)
-            return 0;
-
-        if (dp[i][prev] != -1)
-            return dp[i][prev];
-
-        int skip = f(nums, i + 1, prev, dp);
-        int take = 0;
-        if (prev == 0 || nums[i - 1] > nums[prev - 1])
-            take = 1 + f(nums, i + 1, i, dp);
-
-        return dp[i][prev] = Math.max(skip, take);
-    }
 }
