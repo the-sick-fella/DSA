60fa65ae7f625050f7135120ccea42342a297e9e
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-17 20:45:26 +0530

    Time: 78 ms (26.66%), Space: 47.1 MB (25.68%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,28 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int curr [] = new int [n];
-        curr[0] = nums[0];
-        int idx = 0;
+        int dp[] = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            int temp[] = new int[n + 1];
+            for (int prev = i - 1; prev >= -1; prev--) {
+                int skip = dp[prev + 1];
+                int take = 0;
+                if (prev == -1 || nums[i] > nums[prev]) take = 1 + dp[i + 1];
 
-        for(int i = 1; i<n; i++){
-            if(nums[i] > curr[idx]) curr[++idx] = nums[i];
-            else {
-                int c = ceiling (curr, idx, nums[i]);
-                curr[c] = nums[i];
+                temp[prev + 1] = Math.max(skip, take);
             }
+            dp = temp;
         }
-        return idx+1;
-    }
-
-    int ceiling(int [] temp, int ei, int target){
-        int si = 0;
-        while(si <= ei){
-            int mid = (si + ei)/2;
-            if(temp[mid] == target) return mid;
-            if(temp[mid] > target) ei = mid - 1;
-            else si = mid + 1;
-        }
-        return si;
+        return dp[-1 + 1];
     }
 }
