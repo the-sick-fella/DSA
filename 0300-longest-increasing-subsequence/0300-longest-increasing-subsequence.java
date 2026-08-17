e111959cb111e56f36b07b8f7538da8d2b0c9905
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-17 21:41:41 +0530

    Time: 86 ms (23.93%), Space: 237 MB (17.8%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,7 +4,7 @@ class Solution {
         int dp[][] = new int[n+1][n+1];
         
         for(int i = n-1; i>=0; i--){
-            for(int j = n-2; j>=-1; j--){
+            for(int j = i-1; j>=-1; j--){
                 int skip = dp[i+1][j+1];
                 int take = 0;
                 if(j==-1 || nums[i] > nums[j]) take = 1 + dp[i+1][i+1];
