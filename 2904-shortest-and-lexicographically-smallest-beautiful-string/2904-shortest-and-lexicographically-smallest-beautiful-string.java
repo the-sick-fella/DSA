d577b0b5a95ebe909880335741908d07cb2f6049
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-26 21:02:44 +0530

    Time: 1 ms (100%), Space: 44 MB (75.84%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -11,7 +11,7 @@ class Solution {
         while(ei < s.length()){
             if(s.charAt(ei) == '1') count++;
             if(count == k){
-                ans = findSmallerString(ans, s.substring(si, ei+1));
+                if(foundSmaller(ans, s, si, ei)) ans = s.substring(si, ei+1);
                 si++;
                 count--;
 
@@ -22,10 +22,10 @@ class Solution {
         return ans;
     }
 
-    String findSmallerString(String s1, String s2){
-        if(s1.length() == 0 || s2.length() < s1.length()) return s2;
-        if(s2.length() > s1.length()) return s1;
-        int comp = s1.compareTo(s2);
-        return comp > 0 ? s2 : s1;
+    boolean foundSmaller(String s1, String s, int si, int ei){
+        if(s1.length() == 0 || ei-si+1 < s1.length()) return true;
+        if(ei-si+1 > s1.length()) return false;
+        int comp = s1.compareTo(s.substring(si, ei+1));
+        return comp > 0 ? true : false;
     }
 }
