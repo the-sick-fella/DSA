c7e7578ad140e230abb3dbf4b05b5f1b128fff06
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-26 21:08:15 +0530

    Time: 3 ms (95.12%), Space: 43.8 MB (93.77%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null)
+            return list;
+        TreeMap<Integer, List<int[]>> map = new TreeMap<>();
+        f(map, root, 0, 0);
+
+        for (int key : map.keySet()) {
+            List<int[]> temp = map.get(key);
+
+            Collections.sort(temp, (a, b) -> {
+                if (a[0] != b[0]) {
+                    return a[0] - b[0];
+                }
+                return a[1] - b[1];
+            });
+
+            List<Integer> curr = new ArrayList<>();
+            for (int[] arr : temp) {
+                curr.add(arr[1]);
+            }
+            list.add(curr);
+        }
+
+        return list;
+    }
+
+    void f(TreeMap<Integer, List<int[]>> map, TreeNode node, int row, int col) {
+        List<int[]> curr = map.getOrDefault(col, new ArrayList<>());
+        int temp [] = new int[2];
+        temp[0] = row;
+        temp[1] = node.val;
+        curr.add(temp);
+        map.put(col, curr);
+
+        if (node.left != null)
+            f(map, node.left, row + 1, col - 1);
+        if (node.right != null)
+            f(map, node.right, row + 1, col + 1);
+    }
+}
