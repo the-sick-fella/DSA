449278df40f233e73bc2c489a8de246584d562f7
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-28 21:28:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
@@ -1,33 +1,36 @@
 /* Structure of Binary Tree Node
 class Node {
-    int data;
-    Node left, right;
-
-    Node(int val) {
-        this.data = val;
-        this.left = this.right = null;
-    }
-}*/
-
+	int data;
+	Node left, right;
+	
+	Node(int val) {
+		this.data = val;
+		this.left = this.right = null;
+	}
+} */
+// import java.util.*;
 class Solution {
-    public ArrayList<Integer> leftView(Node root) {
-        // code here
-        ArrayList<Integer> list = new ArrayList<>();
-        if(root == null) return list;
-        
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            
-            for(int i = 1; i<=size; i++){
-                Node node = q.poll();
-                if(i == 1) list.add(node.data);
-                
-                if(node.left != null) q.offer(node.left);
-                if(node.right != null) q.offer(node.right);
-            }
-        }
-        return list;
-    }
+	public ArrayList<Integer> leftView(Node root) {
+		// code here
+		ArrayList<Integer> ans = new ArrayList<>();
+		if (root == null)
+			return ans;
+		Queue <Node> q = new LinkedList<>();
+		q.offer(root);
+		while (!q.isEmpty()) {
+			int size = q.size();
+			Node node = null;
+			for (int i = 1; i <= size; i++) {
+				node = q.poll();
+				if (i == 1)
+					ans.add(node.data);
+				if (node.left != null)
+					q.offer(node.left);
+				if (node.right != null)
+					q.offer(node.right);
+			}
+		}
+		
+		return ans;
+	}
 }
