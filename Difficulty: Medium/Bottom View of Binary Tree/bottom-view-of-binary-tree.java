ca2e15793cb7e000117cda452060e882bc0e381e
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-28 20:06:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
@@ -17,20 +17,25 @@ class Solution {
 	public ArrayList<Integer> bottomView(Node root) {
 		// code here
 		ArrayList<Integer> list = new ArrayList<>();
-		TreeMap<Integer, int[]> map = new TreeMap<>();
-		getNodes(map, root, 0, 0);
-		for (int col : map.keySet()) {
+		int range [] = new int [2];
+		HashMap<Integer, int[]> map = new HashMap<>();
+		getNodes(map, root, 0, 0, range);
+		for (int col = range[0]; col <= range[1]; col++) {
 			list.add(map.get(col)[1]);
 		}
 		return list;
 	}
 	
-	void getNodes(TreeMap<Integer, int[]> map, Node node, int row, int col) {
+	void getNodes(HashMap<Integer, int[]> map, Node node, int row, int col, int[] range) {
 		if (map.get(col) == null || map.get(col)[0] <= row)
 			map.put(col, new int[] {row, node.data});
+		
+		range[0] = Math.min(range[0], col);
+		range[1] = Math.max(range[1], col);
+		
 		if (node.left != null)
-			getNodes(map, node.left, row + 1, col - 1);
+			getNodes(map, node.left, row + 1, col - 1, range);
 		if (node.right != null)
-			getNodes(map, node.right, row + 1, col + 1);
+			getNodes(map, node.right, row + 1, col + 1, range);
 	}
 }
