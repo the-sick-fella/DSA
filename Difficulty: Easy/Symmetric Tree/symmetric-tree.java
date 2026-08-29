e6d3b51eb40bec993c897de62c07defd59caa9de
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-29 21:27:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Symmetric Tree/symmetric-tree.java
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.java
@@ -10,52 +10,20 @@ class Node{
     }
 }
 */
-  class Pair{
-     Node node;
-     int col;
-     public Pair(Node node, int col){
-         this.node = node;
-         this.col = col;
-     }
-  }
 
 class Solution {
     public boolean isSymmetric(Node root) {
         // code here
-         Queue<Pair> q = new LinkedList<>();
-         if(root.left != null) q.offer(new Pair(root.left, -1));
-         if(root.right != null) q.offer(new Pair(root.right, 1));
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
 
-         while(!q.isEmpty()){
-             int size = q.size();
-             List<Pair> list = new ArrayList<>();
-             for(int i = 0; i<size; i++){
-                 Pair p = q.poll();
-                 list.add(p);
-                 Node node = p.node;
-                 int col = p.col;
+    private boolean isMirror(Node n1, Node n2) {
+        if (n1 == null && n2 == null) return true;
+        if (n1 == null || n2 == null) return false;
 
-
-                 if(node.left != null) q.offer(new Pair(node.left, col-1));
-                 if(node.right != null) q.offer(new Pair(node.right, col+1));
-             }
-
-
-             if(!symmetric(list)) return false;
-         }
-
-
-         return true;
-     }
-
-
-     static boolean symmetric(List<Pair> list){
-         if(list.size() % 2 != 0) return false;
-         int i = 0, j = list.size()-1;
-         while(i<j){
-             Pair p1 = list.get(i++), p2 = list.get(j--);
-             if(p1.node.data != p2.node.data || (p1.col + p2.col != 0)) return false;
-         }
-         return true;
-     }
- }
+        return (n1.data == n2.data)
+            && isMirror(n1.left, n2.right)
+            && isMirror(n1.right, n2.left);
+    }
+}
