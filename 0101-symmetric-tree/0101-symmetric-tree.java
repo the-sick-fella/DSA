97896601b1e65d35986820bbbc8ca8f555585f11
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-29 21:25:10 +0530

    Time: 0 ms (100%), Space: 43.5 MB (70.86%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -13,48 +13,15 @@
  *     }
  * }
  */
- class Pair{
-    TreeNode node;
-    int col;
-    public Pair(TreeNode node, int col){
-        this.node = node;
-        this.col = col;
-    }
- }
-
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        if(root.left != null) q.offer(new Pair(root.left, -1));
-        if(root.right != null) q.offer(new Pair(root.right, 1));
-
-        while(!q.isEmpty()){
-            int size = q.size();
-            // int [] nums = new int[q.size()];
-            List<Pair> list = new ArrayList<>();
-            for(int i = 0; i<size; i++){
-                Pair p = q.poll();
-                list.add(p);
-                TreeNode node = p.node;
-                int col = p.col;
-
-                if(node.left != null) q.offer(new Pair(node.left, col-1));
-                if(node.right != null) q.offer(new Pair(node.right, col+1));
-            }
-
-            if(!symmetric(list)) return false;
-        }
-
-        return true;
+        return isSymmetric(root.left, root.right);
     }
 
-    boolean symmetric(List<Pair> list){
-        if(list.size() % 2 != 0) return false;
-        int i = 0, j = list.size()-1;
-        while(i<j){
-            Pair p1 = list.get(i++), p2 = list.get(j--);
-            if(p1.node.val != p2.node.val || (p1.col + p2.col != 0)) return false;
-        }
-        return true;
+    boolean isSymmetric(TreeNode n1, TreeNode n2){
+        if(n1 == null && n2 == null) return true;
+        if(n1 == null || n2 == null || n1.val != n2.val) return false;
+
+        return isSymmetric(n1.left, n2.right) && isSymmetric(n1.right, n2.left);
     }
 }
