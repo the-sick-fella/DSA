7218952e137cab3c86877a621d79e45ad2de5844
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-08-30 22:14:33 +0530

    Time: 14 ms (98.97%), Space: 69.9 MB (35.27%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -7,48 +7,19 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Pair {
-    boolean found;
-    TreeNode node;
-
-    public Pair(boolean found, TreeNode node) {
-        this.found = found;
-        this.node = node;
-    }
-}
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Pair pair = new Pair(false, null);
-        Set<Integer> list = f(root, p, q, pair);
-        return pair.node;
+        return getLCA(root, p, q);
     }
 
-    Set<Integer> f(TreeNode node, TreeNode p, TreeNode q, Pair pair) {
-        Set<Integer> set = new HashSet<>();
-        set.add(node.val);
+    TreeNode getLCA(TreeNode node, TreeNode p, TreeNode q) {
+        if(node == null) return null;
+        if(node == p || node == q) return node;
 
-        if(node.left != null){
-            Set<Integer> ls = f(node.left, p, q, pair);
-            if(pair.found) return set;
-            for(int val : ls) set.add(val);
-        }
-        
-        if(set.contains(p.val) && set.contains(q.val)){
-            pair.found = true;
-            pair.node = node;
-        }
-
-        if(node.right != null){
-            Set<Integer> rs = f(node.right, p, q, pair);
-            if(pair.found) return set;
-            for(int val : rs) set.add(val);
-        }
-
-        if(set.contains(p.val) && set.contains(q.val)){
-            pair.found = true;
-            pair.node = node;
-        }
-        return set;
+        TreeNode left = getLCA(node.left, p, q);
+        TreeNode right = getLCA(node.right, p, q);
+        if(left != null && right != null) return node;
+        return left != null ? left : right;
     }
 }
