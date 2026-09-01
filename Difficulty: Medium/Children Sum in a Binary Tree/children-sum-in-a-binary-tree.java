00d1fba0fcdd2be85163495684ce5e1c6398a9d0
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-01 20:04:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.java
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.java
@@ -13,14 +13,17 @@ class Solution {
         //  code here
         if(root.left == null && root.right == null) return true;
         int left = 0, right = 0;
+        
         if(root.left != null){
             if(!isSumProperty(root.left)) return false;
             left = root.left.data;
         }
+        
         if(root.right != null){
             if(!isSumProperty(root.right)) return false;
             right = root.right.data;
         }
+        
         return root.data == left + right;
     }
 }
