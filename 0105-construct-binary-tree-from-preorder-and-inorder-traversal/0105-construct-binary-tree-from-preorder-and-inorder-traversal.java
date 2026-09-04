7676b3f997aeaef247caa1d6c7fca8a4ce1452d1
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-04 21:34:52 +0530

    Time: 1 ms (97.48%), Space: 46.4 MB (37.98%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,25 +16,21 @@
 class Solution {
     public TreeNode buildTree(int[] pre, int[] in) {
         int n = pre.length;
-        return construct(pre, 0, n - 1, in, 0, n - 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            map.put(in[i], i);
+        }
+        return construct(pre, 0, n - 1, in, 0, n - 1, map);
     }
 
-    TreeNode construct(int[] pre, int p1, int p2, int[] in, int i1, int i2) {
+    TreeNode construct(int[] pre, int p1, int p2, int[] in, int i1, int i2, Map<Integer, Integer> map) {
         if (p1 > p2 || i1 > i2) return null;
         TreeNode node = new TreeNode(pre[p1]);
         if (p1 < p2) {
-            int idx = getIndex(in, i1, i2, pre[p1]);
-            node.left = construct(pre, p1 + 1, p1 + (idx - i1), in, i1, idx - 1);
-            node.right = construct(pre, p1 + (idx - i1) + 1, p2, in, idx + 1, i2);
+            int idx = map.get(pre[p1]);
+            node.left = construct(pre, p1 + 1, p1 + (idx - i1), in, i1, idx - 1, map);
+            node.right = construct(pre, p1 + (idx - i1) + 1, p2, in, idx + 1, i2, map);
         }
         return node;
-    }
-
-    int getIndex(int[] nums, int i1, int i2, int val) {
-        for (; i1 <= i2; i1++) {
-            if (nums[i1] == val)
-                return i1;
-        }
-        return -1;
     }
 }
