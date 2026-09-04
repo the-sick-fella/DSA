1d721baed75169195a83ad23420bce49a03a5cc5
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-04 18:42:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,13 +1,6 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int [] maxA = new int[nums.length];
         int [] minA = new int[nums.length];
-
-        int maxNum = Integer.MIN_VALUE;
-        for(int i = 0; i<nums.length; i++){
-            maxA[i] = Math.max(maxNum, nums[i]);
-            maxNum = Math.max(maxNum, nums[i]);
-        }
 
         int minNum = Integer.MAX_VALUE;
         for(int i = nums.length-1; i>=0; i--){
@@ -15,9 +8,9 @@ class Solution {
             minNum = Math.min(minNum, nums[i]);
         }
 
-        
+        int max = Integer.MIN_VALUE;
         for(int i = 0; i<nums.length; i++){
-            if(maxA[i] - minA[i] <= k) return i;
+            if(Math.max(max, nums[i]) - minA[i] <= k) return i;
         }
 
         return -1;
