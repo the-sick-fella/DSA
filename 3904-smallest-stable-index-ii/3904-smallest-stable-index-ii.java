9b67b4486db0af70b766797642f98641ff16a7e5
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-05 14:14:23 +0530

    Time: 3 ms (100%), Space: 133.1 MB (27.86%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,23 +1,17 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int [] maxA = new int[nums.length];
-        int [] minA = new int[nums.length];
+        int [] arr = new int[nums.length];
 
-        int maxNum = Integer.MIN_VALUE;
-        for(int i = 0; i<nums.length; i++){
-            maxA[i] = Math.max(maxNum, nums[i]);
-            maxNum = Math.max(maxNum, nums[i]);
-        }
-
-        int minNum = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
         for(int i = nums.length-1; i>=0; i--){
-            minA[i] = Math.min(minNum, nums[i]);
-            minNum = Math.min(minNum, nums[i]);
+            min = Math.min(min, nums[i]);
+            arr[i] = min;
         }
-
         
+        int max = Integer.MIN_VALUE;
         for(int i = 0; i<nums.length; i++){
-            if(maxA[i] - minA[i] <= k) return i;
+            max = Math.max(max, nums[i]);
+            if(max - arr[i] <= k) return i;
         }
 
         return -1;
