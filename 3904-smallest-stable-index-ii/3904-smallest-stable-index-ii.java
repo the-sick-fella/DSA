8fae20e5bd3fa84bf63ef350b154f79bc82db744
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-05 14:13:49 +0530

    Time: 6 ms (45.8%), Space: 126.9 MB (48.85%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,17 +1,23 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int [] arr = new int[nums.length];
+        int [] maxA = new int[nums.length];
+        int [] minA = new int[nums.length];
 
-        int min = Integer.MAX_VALUE;
-        for(int i = nums.length-1; i>=0; i--){
-            min = Math.min(min, nums[i]);
-            arr[i] = min;
-        }
-        
-        int max = Integer.MIN_VALUE;
+        int maxNum = Integer.MIN_VALUE;
         for(int i = 0; i<nums.length; i++){
-            max = Math.max(max, nums[i]);
-            if(max - arr[i] <= k) return i;
+            maxA[i] = Math.max(maxNum, nums[i]);
+            maxNum = Math.max(maxNum, nums[i]);
+        }
+
+        int minNum = Integer.MAX_VALUE;
+        for(int i = nums.length-1; i>=0; i--){
+            minA[i] = Math.min(minNum, nums[i]);
+            minNum = Math.min(minNum, nums[i]);
+        }
+
+        
+        for(int i = 0; i<nums.length; i++){
+            if(maxA[i] - minA[i] <= k) return i;
         }
 
         return -1;
