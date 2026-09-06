3d5ce476997c855631d4aafec53383f609a83641
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-06 10:55:22 +0530

    Time: 18 ms (76.88%), Space: 54.2 MB (73.01%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,19 +1,31 @@
 class Solution {
     public int numDistinct(String s, String t) {
         int m = s.length(), n = t.length();
-        int dp [][] = new int [m][n];
-        for(int r[] : dp) Arrays.fill(r, -1);
-        return f(s, t, 0, 0, dp);
+        int dp[][] = new int[m+1][n+1];
+        for(int r[] : dp) r[n] = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int ans = dp[i + 1][j];
+                if (s.charAt(i) == t.charAt(j))
+                    ans += dp[i + 1][j + 1];
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
     }
 
-    int f(String s, String t, int i, int j, int [][] dp) {
-        if(j == t.length()) return 1;
-        if(i == s.length()) return 0;
+    int f(String s, String t, int i, int j, int[][] dp) {
+        if (j == t.length())
+            return 1;
+        if (i == s.length())
+            return 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        int ans = f(s, t, i+1, j, dp);
-        if(s.charAt(i) == t.charAt(j)) ans += f(s, t, i+1, j+1, dp);
+        int ans = f(s, t, i + 1, j, dp);
+        if (s.charAt(i) == t.charAt(j))
+            ans += f(s, t, i + 1, j + 1, dp);
         return dp[i][j] = ans;
     }
 }
