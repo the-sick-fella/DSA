aa6ccd031f6d235045dabfdc0961e14b24cf1523
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-06 10:52:49 +0530

    Time: 21 ms (33.78%), Space: 54.2 MB (73.01%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,22 +3,17 @@ class Solution {
         int m = s.length(), n = t.length();
         int dp [][] = new int [m][n];
         for(int r[] : dp) Arrays.fill(r, -1);
-        StringBuilder sb = new StringBuilder();
-        return f(s, t, 0, 0, sb, dp);
+        return f(s, t, 0, 0, dp);
     }
 
-    int f(String s, String t, int i, int j, StringBuilder sb, int [][] dp) {
+    int f(String s, String t, int i, int j, int [][] dp) {
         if(j == t.length()) return 1;
         if(i == s.length()) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int ans = f(s, t, i+1, j, sb, dp);
-        if(s.charAt(i) == t.charAt(j)){
-            sb.append(s.charAt(i));
-            ans += f(s, t, i+1, j+1, sb, dp);
-            sb.deleteCharAt(sb.length()-1);
-        }
+        int ans = f(s, t, i+1, j, dp);
+        if(s.charAt(i) == t.charAt(j)) ans += f(s, t, i+1, j+1, dp);
         return dp[i][j] = ans;
     }
 }
