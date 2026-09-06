df9590ec167b9af6194ec270210b3b689179d6ac
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-06 10:50:32 +0530

    Time: 39 ms (5.2%), Space: 54.3 MB (62.97%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,17 +1,24 @@
 class Solution {
     public int numDistinct(String s, String t) {
         int m = s.length(), n = t.length();
-        int[] dp = new int[n+1];
-        dp[n]=1;
-        for(int i = m-1; i>=0; i--){
-            int temp [] = new int[n+1];
-            temp[n]=1;
-            for(int j = n-1; j>=0; j--){
-                temp[j] = dp[j];
-                if(s.charAt(i) == t.charAt(j)) temp[j] += dp[j+1];
-            }
-            dp = temp;
+        int dp [][] = new int [m][n];
+        for(int r[] : dp) Arrays.fill(r, -1);
+        StringBuilder sb = new StringBuilder();
+        return f(s, t, 0, 0, sb, dp);
+    }
+
+    int f(String s, String t, int i, int j, StringBuilder sb, int [][] dp) {
+        if(j == t.length()) return 1;
+        if(i == s.length()) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int ans = f(s, t, i+1, j, sb, dp);
+        if(s.charAt(i) == t.charAt(j)){
+            sb.append(s.charAt(i));
+            ans += f(s, t, i+1, j+1, sb, dp);
+            sb.deleteCharAt(sb.length()-1);
         }
-        return dp[0];
+        return dp[i][j] = ans;
     }
 }
