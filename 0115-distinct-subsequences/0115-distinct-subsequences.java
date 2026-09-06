7a33381ecd97ed202e7d924bdbc7a8127c6b1cd7
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-06 10:55:39 +0530

    Time: 18 ms (76.88%), Space: 54.3 MB (62.97%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -13,19 +13,4 @@ class Solution {
         }
         return dp[0][0];
     }
-
-    int f(String s, String t, int i, int j, int[][] dp) {
-        if (j == t.length())
-            return 1;
-        if (i == s.length())
-            return 0;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int ans = f(s, t, i + 1, j, dp);
-        if (s.charAt(i) == t.charAt(j))
-            ans += f(s, t, i + 1, j + 1, dp);
-        return dp[i][j] = ans;
-    }
 }
