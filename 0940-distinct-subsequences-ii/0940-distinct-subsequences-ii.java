6c2356cb246db808d4e13352224ee9acf6907cc7
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-07 22:10:52 +0530

    Time: 4 ms (70.57%), Space: 43.4 MB (41.84%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,15 +1,16 @@
 class Solution {
     public int distinctSubseqII(String s) {
         int total = 0, curr = 0;
-        int f [] = new int[26];
+        int f[] = new int[26];
         char last = '.';
-        int mod = (int)1e9 + 7;
+        int mod = (int) 1e9 + 7;
 
-        for(int i = 0; i<s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            curr = (total + 1 - f[c-'a'] + mod) % mod;
+            curr = total + 1 - f[c - 'a'];
+            if (curr < 0) curr += mod;
             total = (total + curr) % mod;
-            f[c-'a'] = (f[c-'a']  + curr) % mod;
+            f[c - 'a'] = (f[c - 'a'] + curr) % mod;
         }
         return total;
     }
