9733f65b5fbc434e301e46aed5e02ca762382eed
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-09 19:40:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (97.65%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,12 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        int ans = 0;
-
-        while(n>999){
-            ans+=(n-999);
-            n/=1000;
-        }
-
-        return ans;
+        int ans = n-999;
+        return ans > 0 ? ans : 0;
     }
 }
