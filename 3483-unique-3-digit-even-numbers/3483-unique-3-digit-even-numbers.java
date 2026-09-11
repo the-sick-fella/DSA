aa894fca8cf34e46d8c05986936e19108fc51bbf
Syed Hamzah Rahman <112624937+the-sick-fella@users.noreply.github.com>
2026-09-11 20:47:51 +0530

    Time: 1 ms (100%), Space: 43.7 MB (97.73%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,19 +1,24 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        int f [] = new int[10];
-        for(int digit : digits) f[digit]++;
+        int freq [] = new int[10];
+        for(int digit : digits) freq[digit]++;
         int ans = 0;
-        for(int num = 100; num<999; num+=2){
-            int temp [] = new int[10];
-            int n = num;
-            while(n>0){
-                int digit = n%10;
-                temp[digit]++;
-                if(temp[digit] > f[digit]) break;
-                n/=10;
-            }
 
-            if(n==0) ans++;
+        for(int f = 1; f<10; f++){
+            if(freq[f] == 0) continue;
+            freq[f]--;
+
+            for(int s = 0; s<10; s++){
+                if(freq[s] == 0) continue;
+                freq[s]--;
+
+                for(int t = 0; t<9; t+=2){
+                    if(freq[t] != 0) ans++;
+                }
+
+                freq[s]++;
+            }
+            freq[f]++;
         }
         return ans;
     }
